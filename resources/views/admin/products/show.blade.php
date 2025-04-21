f6394a4f8d002c9f129b9ff1d952fd69b042e24b
MythEclipse <asepharyanalm001@gmail.com>
2025-04-21 19:07:58 +0700

    Fix image display logic in product show view

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -5,9 +5,11 @@
 
     <div class="py-6">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 bg-white p-6 rounded shadow">
+            @if($product->image)
+                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="w-64 h-64 object-cover mb-4 rounded">
+            @endif
             <p class="text-gray-700 mb-4">{{ $product->description }}</p>
             <p class="text-sm text-gray-600">Rp{{ number_format($product->price, 0, ',', '.') }}</p>
-
 
             <div class="mt-6">
                 <a href="{{ route('products.edit', $product) }}" class="text-blue-600 hover:underline mr-4">Edit</a>
